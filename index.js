ad7142f17ea8c28de4bb54210ab81a7f08f8d078
Ayako Woollan <ayako.woollan@gmail.com>
2022-01-30 17:12:44 +1100

    cleaning code, Trying to add MarkDownExample

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Including packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown');
+const gm = require('./utils/generateMarkdown');
 
 // Creating an array of questions for user input
 const questions = [
@@ -70,94 +70,26 @@ const questions = [
 ];
 
 // Creating functions to write README file
+const markDownExample = `
+Let's create your READme.md file!
+
+---Mark Down examples--
+*This text will be italic* 
+**This text will be bold**
+Links: [text](https://website.com)
+Image: ![This is a alt text.](/image/sample.png)
+------------------------
+`
 const promptUser = () => inquirer.prompt(questions);
-const generateREADME = ({ userName, projectName, description, installation, technology1, technology2, technology3, usage, license, gitHubName, email, repoName }) =>
-    `
-    #${projectName}
-<details>
-  <summary>Table of Contents</summary>
-  <ol>
-    <li>
-      <a href="#about-the-project">About The Project</a>
-      <ul>
-        <li><a href="#built-with">Built With</a></li>
-      </ul>
-    </li>
-    <li>
-      <a href="#getting-started">Getting Started</a>
-      <ul>
-        <li><a href="#installation">Installation</a></li>
-      </ul>
-    </li>
-    <li><a href="#usage">Usage</a></li>
-    <li><a href="#license">License</a></li>
-    <li><a href="#contact">Contact</a></li>
-  </ol>
-</details>
 
-
-
-<!-- ABOUT THE PROJECT -->
-## About The Project
-
-[![Product Name Screen Shot][product-screenshot]](./assets/screenshot/product.png)
-
-${description}
-
-<p align="right">(<a href="#top">back to top</a>)</p>
-
-
-
-### Built With
-
-* ${technology1}
-* ${technology2}
-* ${technology3}
-
-<p align="right">(<a href="#top">back to top</a>)</p>
-
-## Getting Started
-To get a local copy up and running follow these simple example steps.
-
-  
-### Installation
-${installation}
-
-
-<p align="right">(<a href="#top">back to top</a>)</p>
-
-## Usage
-${usage}
-
-Following animation and images show this application's functionality.
-[![Product Screen Shot][product-screenshot]](./assets/screenshot/product.gif)
-
-
-<p align="right">(<a href="#top">back to top</a>)</p>
-
-
-
-## License
-
-Distributed under ${license} License. See 'LICENSE.txt' for more information.
-
-<p align="right">(<a href="#top">back to top</a>)</p>
-
-
-
-## Contact Me
-
-${userName} - ${email}
-
-Project Link: [https://github.com/${gitHubName}/${repoName}](https://github.com/${gitHubName}/${repoName})
-
-<p align="right">(<a href="#top">back to top</a>)</p>
-`;
 
 // Creating a function to initialize app
 const init = () => {
     promptUser()
-        .then((answers) => fs.writeFileSync('README.md', generateREADME(answers)))
+        .then((answers) => {
+            console.log(markDownExample);
+            fs.writeFileSync('README.md', gm.generateMarkdown(answers))
+        })
         .then(() => console.log('Your README file is successfully created.'))
         .catch((err) => console.error(err));
 }
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,21 +1,98 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+//Creating a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) { }
-if ()
 
-  // TODO: Create a function that returns the license link
-  // If there is no license, return an empty string
-  function renderLicenseLink(license) { }
+//Creating a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) { }
 
-// TODO: Create a function that returns the license section of README
+//Creating a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) { }
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+//Creating a function to generate markdown for README
+const generateMarkdown = ({ userName, projectName, description, installation, technology1, technology2, technology3, usage, license, gitHubName, email, repoName }) =>
+  `
+    #${projectName}
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#installation">Installation</a></li>
+      </ul>
+    </li>
+    <li><a href="#usage">Usage</a></li>
+    <li><a href="#license">License</a></li>
+    <li><a href="#contact">Contact</a></li>
+  </ol>
+</details>
 
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+[![Product Name Screen Shot][product-screenshot]](./assets/screenshot/product.png)
+
+${description}
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+### Built With
+
+* ${technology1}
+* ${technology2}
+* ${technology3}
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+## Getting Started
+To get a local copy up and running follow these simple example steps.
+
+  
+### Installation
+${installation}
+
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+## Usage
+${usage}
+
+Following animation and images show this application's functionality.
+[![Product Screen Shot][product-screenshot]](./assets/screenshot/product.gif)
+
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+## License
+
+Distributed under ${license} License. See 'LICENSE.txt' for more information.
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+## Contact Me
+
+${userName} - ${email}
+
+Project Link: [https://github.com/${gitHubName}/${repoName}](https://github.com/${gitHubName}/${repoName})
+
+<p align="right">(<a href="#top">back to top</a>)</p>
 `;
-}
+
 
 module.exports = generateMarkdown;
