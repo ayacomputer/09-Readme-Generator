9b0e3256defd94c9127130176586c4f1a9d860b1
Ayako Woollan <ayako.woollan@gmail.com>
2022-01-30 13:01:02 +1100

    updating README structure

--- index.js
+++ index.js
@@ -6,13 +6,28 @@ const inquirer = require('inquirer');
 const questions = [
     {
         type: 'input',
+        message: 'What is your name?',
+        name: 'userName',
+    },
+    {
+        type: 'input',
         message: 'What is your project name?',
         name: 'projectName',
     },
     {
         type: 'input',
+        message: 'What is your repository name?',
+        name: 'repoName',
+    },
+    {
+        type: 'input',
         message: 'Describe your project?',
         name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What technologies(languages) did you use to build this application?',
+        name: 'technologies',
     },
     {
         type: 'input',
@@ -46,7 +61,113 @@ const questions = [
 // Creating functions to write README file
 const promptUser = () => inquirer.prompt(questions);
 const generateREADME = ({ projectName, description, installation, usage, license, gitHubName, email }) => {
+    `<!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#prerequisites">Prerequisites</a></li>
+        <li><a href="#installation">Installation</a></li>
+      </ul>
+    </li>
+    <li><a href="#usage">Usage</a></li>
+    <li><a href="#roadmap">Roadmap</a></li>
+    <li><a href="#contributing">Contributing</a></li>
+    <li><a href="#license">License</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#acknowledgments">Acknowledgments</a></li>
+  </ol>
+</details>
 
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+[![Product Name Screen Shot][product-screenshot]](./assets/screenshot/product.png)
+
+${description}
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+### Built With
+
+* ${technology}
+* [Next.js](https://nextjs.org/)
+* [React.js](https://reactjs.org/)
+* [Vue.js](https://vuejs.org/)
+* [Angular](https://angular.io/)
+* [Svelte](https://svelte.dev/)
+* [Laravel](https://laravel.com)
+* [Bootstrap](https://getbootstrap.com)
+* [JQuery](https://jquery.com)
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+
+### Installation
+
+
+// Prep. Get a free API Key at [https://example.com](https://example.com)
+
+1. Clone the repo
+   ```sh
+   git clone https://github.com/your_username_/Project-Name.git
+    ```
+2. Install NPM packages
+   ```sh
+   npm install
+        ```
+3. Enter your API in `config.js`
+   ```js
+    const API_KEY = 'ENTER YOUR API';
+    ```
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- USAGE -->
+## Usage
+
+Use this space to show useful examples of how a project can be used. Additional screenshots, code examples and demos work well in this space. You may also link to more resources.
+
+_For more examples, please refer to the [Documentation](https://example.com)_
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- LICENSE -->
+## License
+
+Distributed under ${license} License. See `LICENSE.txt` for more information.
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- CONTACT -->
+## Contact Me
+
+${userName} - ${email}
+
+Project Link: [https://github.com/${gitHubName}/${repoName}](https://github.com/${gitHubName}/${repoName})
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+`
 }
 
 // Creating a function to initialize app
@@ -54,7 +175,7 @@ const init = () => {
     promptUser()
         .then((answers) => fs.writeFileSync('README.md', generateREADME(answers)))
         .then(() => console.log('Your README file is successfully created.'))
-        .catch((error) => console.error(err));
+        .catch((err) => console.error(err));
 }
 
 // Function call to initialize app
